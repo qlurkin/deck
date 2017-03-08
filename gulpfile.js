45e442bf1b73376d7def1fa13b02f511cc092342
Quentin Lurkin <qlurkin@gmail.com>
2017-03-09 00:43:41 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var inliner = require('./gulp-inliner.js');
+
+gulp.task('default', function(){
+  return gulp.src('slides.pug')
+    .pipe(pug())
+    .pipe(inliner())
+    .pipe(gulp.dest('build'))
+});
