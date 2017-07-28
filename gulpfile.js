6d253d8752268d126aa67b57a57fc2d145fe00a8
Quentin Lurkin <qlurkin@gmail.com>
2017-07-28 11:52:25 +0200

    Indentation Uniformization

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var pug = require('gulp-pug');
 var inliner = require('gulp-inliner');
 
 gulp.task('default', function(){
-  return gulp.src('slides.pug')
-    .pipe(pug())
-    .pipe(inliner({inlinemin: true}))
-    .pipe(gulp.dest('build'))
+	return gulp.src('slides.pug')
+		.pipe(pug())
+		.pipe(inliner({inlinemin: true}))
+		.pipe(gulp.dest('build'))
 });
 
 //gulp.watch('*.pug', ['default']);
