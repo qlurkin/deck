f839f10de247851704a33b15060b6c96c64d0cc0
Quentin Lurkin <qlurkin@gmail.com>
2021-03-10 15:12:54 +0100

    Fix Math

--- math.js
+++ math.js
@@ -1,4 +1,4 @@
-import {loadScript, loadCSS, DOMReady} from './helpers'
+import {loadScript} from './helpers'
 
 export function renderMath() {
 	const content = document.body.innerText
@@ -6,16 +6,8 @@ export function renderMath() {
 	if(content.includes('$$') ||
 		(content.includes('\\(') && content.includes('\\)')) ||
 		(content.includes('\\[') && content.includes('\\]'))) {
-			console.log('Loading Katex...')
-			const proms = []
-			loadCSS('https://cdn.jsdelivr.net/npm/katex@0.7.1/dist/katex.min.css')
-			proms.push(loadScript('https://cdn.jsdelivr.net/npm/katex@0.7.1/dist/katex.min.js'))
-			proms.push(loadScript('https://cdn.jsdelivr.net/npm/katex@0.7.1/dist/contrib/auto-render.min.js'))
-			proms.push(DOMReady())
-			Promise.all(proms).then(() => {
-				console.log('Render Math...')
-				renderMathInElement(document.body)
-			})
+			console.log('Loading MathJax...')
+			loadScript('https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js')
 		}
 }
 
